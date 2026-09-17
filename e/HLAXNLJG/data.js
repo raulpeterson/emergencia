12d24ee4190d70a98270c0a16c35792267fe0732
raulpeterson <raulpeterson@gmail.com>
2026-09-17 13:49:28 -0300

    Update data.js

--- e/HLAXNLJG/data.js
+++ e/HLAXNLJG/data.js
@@ -8,24 +8,30 @@ const ficha = {
   "medicamentos": "",
   "condicoes": "",
   "orientacoes": "",
-  "convenio": "",
+  "convenio": "Unimed",
   "contatos": [
     {
       "nome": "Jessica Borges Schwarz",
       "parentesco": "Mãe",
-      "telefone": "+5545999849640",
+      "telefone": "+5545999265020",
       "whatsapp": true
     },
     {
       "nome": "Raul Peterson Horn Schwarz",
       "parentesco": "Pai",
-      "telefone": "+5545999265020",
+      "telefone": "+5545999849640",
       "whatsapp": true
     },
     {
       "nome": "Ramiro Borges da Silveira  ",
       "parentesco": "Avô",
-      "telefone": "+5545999849640",
+      "telefone": "+5545999351620",
+      "whatsapp": true
+    },
+     {
+      "nome": "Maria José da Silva Silveira",
+      "parentesco": "Avó",
+      "telefone": "+5545999896719",
       "whatsapp": true
     }
   ]
